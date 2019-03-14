bc3469cca5cf637585c767c2681b65bb6552c96e
JordanLy <long.lythien@gmail.com>
2019-03-14 14:26:50 +1100

    Adding SearchView and RecyclerView to the fragment_search layout

--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/SearchFragment.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/SearchFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 public class SearchFragment extends Fragment
 {
     // defined variables
+    SearchView searchView;
 
 
     @Nullable
@@ -18,11 +20,13 @@ public class SearchFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
-
-
         return view;
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        searchView = (SearchView) view.findViewById(R.id.searchProduct);
+        searchView.setIconified(false);
+    }
 }
