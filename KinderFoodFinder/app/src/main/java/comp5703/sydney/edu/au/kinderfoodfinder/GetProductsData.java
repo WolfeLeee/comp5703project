9d9dbcb36b6988ce36c6623ae673d15974d58413
JordanLy <long.lythien@gmail.com>
2019-03-20 22:08:11 +1100

    Connect the local database with searchfunction, Users can now use the searchbox to search for brand based on brand title

--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/GetProductsData.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/GetProductsData.java
@@ -1,5 +1,8 @@
 package comp5703.sydney.edu.au.kinderfoodfinder;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -13,14 +16,16 @@ public class GetProductsData extends AsyncTask<String, Void, List<Products>> {
     private static final String TAG = "GetProductsData";
     private List<Products> productsList;
     private final OnDataAvailable mCallback;
+    private final Context mContext;
 
     interface OnDataAvailable{
         void onDataAvailable(List<Products> data);
     }
 
-    public GetProductsData(OnDataAvailable mCallback) {
+    public GetProductsData(OnDataAvailable mCallback, final Context context) {
         this.mCallback = mCallback;
         productsList = new ArrayList<Products>();
+        mContext = context;
     }
 
     @Override
@@ -34,10 +39,41 @@ public class GetProductsData extends AsyncTask<String, Void, List<Products>> {
     @Override
     protected List<Products> doInBackground(String... strings) {
         Log.d(TAG, "doInBackground: starts");
-        Products product1 = new Products("Chicken","Good");
-        Products product2 = new Products("Pork","Avoid");
-        productsList.add(product1);
-        productsList.add(product2);
+
+        String[] projection = {ProductsContract.Columns._ID,
+                ProductsContract.Columns.BRAND_NAME,
+                ProductsContract.Columns.BRAND_RATING,
+                ProductsContract.Columns.BRAND_SORTORDER};
+        ContentResolver contentResolver = mContext.getContentResolver();
+        String selection = ProductsContract.Columns.BRAND_NAME+" LIKE '%"+strings[0]+"%' " ;
+        Cursor cursor = contentResolver.query(ProductsContract.CONTENT_URI, projection, selection, null, ProductsContract.Columns.BRAND_SORTORDER);
+
+        if (cursor != null) {
+            Log.d(TAG, "doInBackground: number of rows: "+cursor.getCount());
+
+            while (cursor.moveToNext()) {
+                String brandname =null;
+                String rating = null;
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    Log.d(TAG, "doInBackground: "+ cursor.getColumnName(i) + ": " + cursor.getString(i));
+                    if(cursor.getColumnName(i).equalsIgnoreCase("Brand")){
+                        brandname = cursor.getString(i);
+                    }
+                    else if (cursor.getColumnName(i).equalsIgnoreCase("Rating")){
+                        rating = cursor.getString(i);
+                    }
+                }
+                if(brandname != null & brandname.length() >0 && rating != null && brandname.length() >0){
+                    Products products = new Products(brandname,rating);
+                    productsList.add(products);
+                }
+                Log.d(TAG, "doInBackground: ======================================================");
+            }
+        }
+//        Products product1 = new Products("Chicken","Good");
+//        Products product2 = new Products("Pork","Avoid");
+//        productsList.add(product1);
+//        productsList.add(product2);
         Log.d(TAG, "doInBackground: ends");
         return productsList;
     }
--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/SearchFragment.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/SearchFragment.java
@@ -1,7 +1,10 @@
 package comp5703.sydney.edu.au.kinderfoodfinder;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -31,46 +34,45 @@ public class SearchFragment extends Fragment implements GetProductsData.OnDataAv
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
         Log.d(TAG, "onViewCreated: starts");
-
+        SearchManager searchManager = (SearchManager) getContext().getSystemService(Context.SEARCH_SERVICE);
+        SearchableInfo searchableInfo =searchManager.getSearchableInfo(getActivity().getComponentName());
         searchView = (SearchView) view.findViewById(R.id.searchProduct);
+        searchView.setSearchableInfo(searchableInfo);
         searchView.setIconified(false);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit: "+query);
+                GetProductsData getProductsData = new GetProductsData(SearchFragment.this,SearchFragment.this.getContext());
+                getProductsData.execute(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+
+
+
+
+
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.productRecyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 //        recyclerView.addOnItemTouchListener(new RecyclerItemClicklistener(getActivity(),recyclerView,getActivity()));
-
         mProductsRecyclerViewAdapter = new ProductsRecyclerViewAdapter(new ArrayList<Products>(), getContext());
         recyclerView.setAdapter(mProductsRecyclerViewAdapter);
 
 
-        String[] projection = {ProductsContract.Columns._ID,
-                ProductsContract.Columns.BRAND_NAME,
-                ProductsContract.Columns.BRAND_RATING,
-                ProductsContract.Columns.BRAND_SORTORDER};
-        ContentResolver contentResolver = getContext().getContentResolver();
 
-        ContentValues values = new ContentValues();
-        values.put(ProductsContract.Columns.BRAND_NAME, "Beef loin chop 200kg");
-        values.put(ProductsContract.Columns.BRAND_RATING, "Best");
-        values.put(ProductsContract.Columns.BRAND_SORTORDER, "1");
-        Uri uri = contentResolver.insert(ProductsContract.CONTENT_URI, values);
-
-        Cursor cursor = contentResolver.query(ProductsContract.CONTENT_URI, projection, null, null, ProductsContract.Columns.BRAND_SORTORDER);
-
-        if (cursor != null) {
-            Log.d(TAG, "onViewCreated: number of rows: " + cursor.getCount());
-            while (cursor.moveToNext()) {
-                for (int i = 0; i < cursor.getColumnCount(); i++) {
-                    Log.d(TAG, "onViewCreated: " + cursor.getColumnName(i) + ": " + cursor.getString(i));
-                }
-                Log.d(TAG, "onViewCreated: =================================================");
-            }
-        }
 
         Log.d(TAG, "onViewCreated: ends");
-
         return view;
     }
 
@@ -78,8 +80,7 @@ public class SearchFragment extends Fragment implements GetProductsData.OnDataAv
     public void onResume()
     {
         super.onResume();
-        GetProductsData getProductsData = new GetProductsData(this);
-        getProductsData.execute("something");
+
     }
 
     @Override
