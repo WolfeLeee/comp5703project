ef57a65745c1d7e58356b6152bbd26420b07b3a0
JordanLy <long.lythien@gmail.com>
2019-03-25 21:02:25 +1100

    Add image columns to local database. Users could search for a product and image of that product will appear next to the title and category

--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/MainActivity.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/MainActivity.java
@@ -1,7 +1,9 @@
 package comp5703.sydney.edu.au.kinderfoodfinder;
 
+import android.Manifest;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,6 +26,10 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.INTERNET},
+                1);
 
         // set up
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
