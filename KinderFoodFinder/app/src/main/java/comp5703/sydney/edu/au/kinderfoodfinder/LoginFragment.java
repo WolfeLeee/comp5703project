f5e832bd077ac35bb9f482878ff6895e52e31069
Wolfe <tlee6405@uni.sydney.edu.au>
2019-04-16 17:30:31 +1000

    Added the functions in login and register fragments that a user now can register and this will send user info to the server and stored into server's databse if valid. Also, user can login and this will send the login info to the server for checking if they are valid and have been registered into the server's database.

--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/LoginFragment.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/LoginFragment.java
@@ -1,16 +1,26 @@
 package comp5703.sydney.edu.au.kinderfoodfinder;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LoginFragment extends Fragment
 {
@@ -20,6 +30,8 @@ public class LoginFragment extends Fragment
     private TextView textRegister;
     private EditText inputEmail, inputPwd;
 
+    private ProgressDialog loginProgressDialog;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
@@ -28,6 +40,8 @@ public class LoginFragment extends Fragment
 
         // set up
         fragmentRegister = new RegisterFragment();
+        loginProgressDialog = new ProgressDialog(getActivity());
+
         btnLogin = (Button) view.findViewById(R.id.btnLogin);
         textRegister = (TextView) view.findViewById(R.id.textRegister);
         inputEmail = (EditText) view.findViewById(R.id.inputEmail);
@@ -39,16 +53,12 @@ public class LoginFragment extends Fragment
             @Override
             public void onClick(View v)
             {
-                // only for testing *****
+                // set up
                 String email = inputEmail.getText().toString().trim();
                 String pwd = inputPwd.getText().toString().trim();
-                if(email.equals("test") && pwd.equals("test"))
-                {
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    startActivity(intent);
-                    getActivity().finish();
-                }
 
+                // login function
+                loginUser(email, pwd);
             }
         });
 
@@ -66,5 +76,68 @@ public class LoginFragment extends Fragment
         });
 
         return view;
+    }
+
+    /* * * * * * * * * *
+     * Login Functions *
+     * * * * * * * * * */
+    private void loginUser(String email, String password)
+    {
+        // show the progress dialog until the login validation is complete
+        loginProgressDialog.setMessage("Login...");
+        loginProgressDialog.show();
+
+        // only for developer to test the app
+        if(email.equals("test") && password.equals("test"))
+        {
+            loginProgressDialog.dismiss();
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+            return;
+        }
+
+        // set up
+        String ipAddress = "172.20.10.4";  //100.101.72.250 Here should be changed to your server IP
+        String url = "http://" + ipAddress + ":3000/android-app-login?email=" + email + "&password=" + password;
+
+        // send the request to the server for checking user login info
+        RequestQueue ExampleRequestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest ExampleStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
+        {
+            @Override
+            public void onResponse(String response)
+            {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+                //You can test it by printing response.substring(0,500) to the screen.
+                loginProgressDialog.dismiss();
+                if(response.equals("Yes"))
+                {
+                    Toast.makeText(getActivity(), "Login Successfully!", Toast.LENGTH_SHORT).show();
+                    Log.d("Send query response:", response);
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+                else
+                {
+                    Toast.makeText(getActivity(), "Wrong email or password!", Toast.LENGTH_SHORT).show();
+                    Log.d("Send query response:", response);
+                }
+            }
+        },
+                new Response.ErrorListener()  //Create an error listener to handle errors appropriately.
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        //This code is executed if there is an error.
+                        loginProgressDialog.dismiss();
+                        Toast.makeText(getActivity(), "Login Failed!", Toast.LENGTH_SHORT).show();
+                        Log.d("Send query error:", error.toString());
+                    }
+                });
+        ExampleRequestQueue.add(ExampleStringRequest);
     }
 }
