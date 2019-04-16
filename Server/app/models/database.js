78c0bb8a17fe752f37c29302139a284f84a50575
Wolfe <tlee6405@uni.sydney.edu.au>
2019-04-17 07:10:54 +1000

    Removed the old server folder

--- Server/app/models/database.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/KinderFoodFinder',function(){
-	console.log('mongodb connected')
-});
-
-module.exports = mongoose;
-
-
--- Server/app/router/Storerouter.js
+++
@@ -1,9 +0,0 @@
-var express=require('express');
-var controller=require('../controller/Storecontroller.js');
-var router=express.Router();
-
-router.get('/',controller.storeswithspecificbrand);
-router.post('/savestore',controller.savestorelocation);
-
-
-module.exports=router;
