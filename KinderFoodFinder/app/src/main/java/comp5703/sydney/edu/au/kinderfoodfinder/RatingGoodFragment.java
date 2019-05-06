59011f3ee46f874189660606d616dea85c2846c5
MingyiGao <48273036+MingyiGao@users.noreply.github.com>
2019-05-06 14:28:16 +1000

    updated locate and rating page

--- KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/RatingGoodFragment.java
+++ KinderFoodFinder/app/src/main/java/comp5703/sydney/edu/au/kinderfoodfinder/RatingGoodFragment.java
@@ -7,12 +7,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class RatingGoodFragment extends Fragment {
+    TextView tv_title_hen, tv_title_chicken, tv_title_pigs;
+    TextView hens_good, chickens_good, pigs_good;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rating_good, container, false);
+
+        tv_title_chicken = view.findViewById(R.id.title_chickens_good);
+        tv_title_hen = view.findViewById(R.id.title_hens_good);
+        tv_title_pigs = view.findViewById(R.id.title_pigs_good);
+
+        hens_good = view.findViewById(R.id.hens_good);
+        chickens_good = view.findViewById(R.id.chickens_good);
+        pigs_good = view.findViewById(R.id.pigs_good);
         return view;
 
     }
